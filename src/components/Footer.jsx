eb6be7955e614fef6ab1920200f59d22af6516b9
Saadat1234 <ahmadkhan.saadat@gmail.com>
2018-10-30 13:35:49 +0100

    add the portofolio footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Footer = (props) => (
+  <div className="fixed-bottom text-center sitefooter">
+   <p>&copy; 2018 AhmadKhan Saadat</p>
+ </div>
+);
+
+export default Footer
